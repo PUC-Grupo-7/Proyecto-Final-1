dc7f97b103a35c17b19d5423baa3c23800d02b15
Javier Vidal <jhonathanv94@gmail.com>
2025-01-13 14:43:37 -0300

    cambios

--- movie_bot/app.py
+++ movie_bot/app.py
@@ -135,4 +135,4 @@ def perfil():
         return "Ha ocurrido un error interno en el servidor.", 500
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=False, host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
